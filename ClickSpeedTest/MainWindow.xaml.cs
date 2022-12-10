27c9b94d4cefbe3744a9212dbe1625bcf9871e46
Sasha <oleksandrsy@gmail.com>
2022-12-10 22:22:16 +0200

    edit tbString and btStart

--- ClickSpeedTest/MainWindow.xaml.cs
+++ ClickSpeedTest/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,8 +24,27 @@ namespace ClickSpeedTest
         public MainWindow()
         {
             InitializeComponent();
+            tbString.Text = File.ReadAllText("C:\\Users\\Саша\\source\\repos\\ClickSpeedTest\\ClickSpeedTest\\Texts\\Hancock.txt");
         }
 
-       
+        private void Window_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            if (e.Text == tbString.Text[0].ToString())
+                tbString.Text = tbString.Text.Remove(0,1);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (btStart.Content == "Start")
+            {
+                btStart.Content = "Finish";
+                btStart.Background = Brushes.Red;
+            }
+            else if (btStart.Content == "Finish")
+            {
+                btStart.Content = "Finish";
+                btStart.Background = Brushes.Red;
+            }
+        }
     }
 }
