b5847b65bd6209c9f7dddb087efe2c544aa0e949
Sasha <oleksandrsy@gmail.com>
2022-12-11 14:54:12 +0200

    finish processing correct button color

--- ClickSpeedTest/MainWindow.xaml.cs
+++ ClickSpeedTest/MainWindow.xaml.cs
@@ -24,16 +24,45 @@ namespace ClickSpeedTest
         private bool TestIsStarted => btStart.Content.ToString() == "Start";
         private Button ChangedButton { get; set; }
         private Brush ChangedButtonStartColor { get; set; }
+        private Brush ShiftButtonStartColor { get; set; }
+        private Dictionary<char, string> SpecialSymbols { get; set; }
         public MainWindow()
         {
             InitializeComponent();
+            ShiftButtonStartColor = btShift.Background;
             tbString.Text = File.ReadAllText("C:\\Users\\Саша\\source\\repos\\ClickSpeedTest\\ClickSpeedTest\\Texts\\Hancock.txt");
+            SpecialSymbols = new Dictionary<char, string>();
+            AddSpecialSymbolsToCollection();
         }
 
         private void Window_PreviewTextInput(object sender, TextCompositionEventArgs e)
         {
             if (e.Text == tbString.Text[0].ToString())
                 tbString.Text = tbString.Text.Remove(0,1);
+        }
+
+        private void AddSpecialSymbolsToCollection()
+        {
+            SpecialSymbols.Add('!', "1");
+            SpecialSymbols.Add('@', "2");
+            SpecialSymbols.Add('#', "3");
+            SpecialSymbols.Add('$', "4");
+            SpecialSymbols.Add('%', "5");
+            SpecialSymbols.Add('^', "6");
+            SpecialSymbols.Add('&', "7");
+            SpecialSymbols.Add('*', "8");
+            SpecialSymbols.Add('(', "9");
+            SpecialSymbols.Add(')', "0");
+            SpecialSymbols.Add('_', "-");
+            SpecialSymbols.Add('+', "=");
+            SpecialSymbols.Add('{', "[");
+            SpecialSymbols.Add('}', "]");
+            SpecialSymbols.Add(':', ";");
+            SpecialSymbols.Add('"', "'");
+            SpecialSymbols.Add('|', "\\");
+            SpecialSymbols.Add('<', ",");
+            SpecialSymbols.Add('>', ".");
+            SpecialSymbols.Add('?', "/");
         }
 
         private void DisplayCorrectButton()
@@ -47,24 +76,127 @@ namespace ClickSpeedTest
         {
             if(ChangedButton != null)
             ChangedButton.Background = ChangedButtonStartColor;
+
+            btShift.Background = ShiftButtonStartColor;
         }
 
         private void SetWhiteBackgroungInCorrectButton()
         {
-            foreach (Grid grid in Keyboard.Children)
+            if (CurrentSymbolIsNumber())
             {
-                foreach (Button button in grid.Children)
+                foreach (Button button in ((Grid)Keyboard.Children[0]).Children)
                 {
                     if (tbString.Text[0].ToString() == button.Content.ToString())
                     {
+                        MakeCorrectButtonWhite(button);
+                        return;
+                    }
+                }
+            }
+            else if (CurrentSymbolIsLetterInLowerCase() || CurrentSymbolIsSpecialSymbolInLowerCase())
+            {
+                foreach (Grid grid in Keyboard.Children)
+                {
+                    foreach (Button button in grid.Children)
+                    {
+                        if (tbString.Text[0].ToString() == button.Content.ToString())
+                        {
+                            MakeCorrectButtonWhite(button);
+                            return;
+                        }
+                    }
+                }
+            }
+            else if (CurrentSymbolIsLetterInUpperCase())
+            {
+                foreach (Grid grid in Keyboard.Children)
+                {
+                    foreach (Button button in grid.Children)
+                    {
+                        if (tbString.Text[0].ToString().ToLower() == button.Content.ToString())
+                        {
+                            MakeCorrectButtonWhite(button);
+                            btShift.Background = Brushes.White;
+                            return;
+                        }
+                    }
+                }
+            }
+            else if (tbString.Text[0] == ' ')
+            {
+                foreach (Button button in ((Grid)Keyboard.Children[4]).Children)
+                {
+                    if (button.Content.ToString() == "Space")
+                    {
+                        MakeCorrectButtonWhite(button);
+                        return;
+                    }
+                       
+                }
+            }
+            else
+            {
+                foreach (var item in SpecialSymbols)
+                {
+                    if (item.Key == tbString.Text[0])
+                    {
+                        foreach (Grid grid in Keyboard.Children)
+                        {
+                            foreach (Button button in grid.Children)
+                            {
+                                if (item.Value == button.Content.ToString())
+                                {
+                                    MakeCorrectButtonWhite(button);
+                                    btShift.Background = Brushes.White;
+                                    return;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private bool CurrentSymbolIsLetterInUpperCase()
+            => (int)tbString.Text[0] >= 65 && (int)tbString.Text[0] <= 90;
+
+        private bool CurrentSymbolIsSpecialSymbolInLowerCase()
+            => (tbString.Text[0] == ',' ||
+            tbString.Text[0] == '.' ||
+            tbString.Text[0] == '.' ||
+            tbString.Text[0] == '/' ||
+            tbString.Text[0] == ';' ||
+            tbString.Text[0] == '\'' ||
+            tbString.Text[0] == '[' ||
+            tbString.Text[0] == ']' ||
+            tbString.Text[0] == '=' ||
+            tbString.Text[0] == '-' ||
+            tbString.Text[0] == '`');
+
+        private bool CurrentSymbolIsLetterInLowerCase()
+           => (int)tbString.Text[0] >= 97 && (int)tbString.Text[0] <= 122;
+
+        private void MakeCorrectButtonWhite(Button button)
+        {
+            ChangedButtonStartColor = button.Background;
+            ChangedButton = button;
+            button.Background = Brushes.White;
+        }
+
+        private bool CurrentSymbolIsNumber() => (int)tbString.Text[0] >= 48 && (int)tbString.Text[0] <= 57;
+
+        /*else if (tbString.Text[0].ToString().ToLower() == button.Content.ToString())
+                    {
+                        btShift.Background = Brushes.White;
                         ChangedButtonStartColor = button.Background;
                         ChangedButton = button;
                         button.Background = Brushes.White;
                         return;
                     }
-                }
-            }
-        }
+                    else
+                    {
+                        CheckSpecialSymbols();
+                    }*/
 
         private void btStart_Click(object sender, RoutedEventArgs e)
         {
